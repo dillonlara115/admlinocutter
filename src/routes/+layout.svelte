9e0ba12080abae00f55ea18b43717d2012dfd4ec
Dillon <dillonlara115@gmail.com>
2025-03-14 13:29:43 -0600

    initial commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+	
 	import '../app.css';
 	let { children } = $props();
 </script>
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap');
+	</style>
 
 {@render children()}
