55b67a2b8aa9a595852b6530227b784aa3fa1d3c
Dillon <dillonlara115@gmail.com>
2025-03-18 16:35:24 -0600

    content additions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,12 @@
       {
         title: "Queen",
         img: "/images/image-1.jpg",
-        description: "A short description for your design."
+        description: ""
       },
       {
-        title: "Arctic Monkeys Poster",
+        title: "bird on the post",
         img: "/images/image-2.jpg",
-        description: "Another example item."
+        description: ""
       },
       {
         title: "666",
@@ -17,14 +17,14 @@
         description: ""
       },
       {
-        title: "Featured Poster",
+        title: "Skeleton Shades",
         img: "/images/skeleton-shades.jpg",
-        description: "Some details about this piece."
+        description: ""
       },
       {
-        title: "Featured Poster",
+        title: "Exchange in the Membrane",
         img: "/images/untitled.jpg",
-        description: "Some details about this piece."
+        description: ""
       },
       {
         title: "See the cat. See the cradle",
@@ -42,94 +42,134 @@
   
   <div class="container mx-auto p-4">
     <!-- Hero / Header Section -->
-    <header class="text-center mb-10">
-      <h1 class="text-4xl mb-2">ADM the Linocutter</h1>
-      <p class="text-gray-600">A short subtitle or tagline here.</p>
-    </header>
+   
+    <div
+    class="hero h-96"
+    style="background-image: url(/images/exchange.jpg);">
+    <div class="hero-overlay"></div>
+    <div class="hero-content text-neutral-content text-center">
+      <div class="max-w-lg">
+        <h1 class="mb-5 mt-5 text-5xl font-bold">ADM the Linocutter</h1>
+        <h2 class="mb-6 text-2xl font-bold">Exchange in the Membrane</h2>
 
-    <section id="newsletter" class="bg-base-200 p-6 rounded-lg">
-        <h2 class="text-2xl font-bold mb-4 text-center">Get The Newsletter</h2>
-        <p class="text-center mb-6">Sign up with to receive the latest about new work</p>
-    
-        <!-- Mailchimp Embed Form -->
-        <div class="flex justify-center">
-          <!-- This is a placeholder. Replace with your actual Mailchimp embed code. -->
-          <div id="mc_embed_shell">
-            <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
-        <style type="text/css">
-              #mc_embed_signup{ false;clear:left; font:14px Helvetica,Arial,sans-serif; max-width: 600px;}
-              /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-                 We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-      </style>
-      <div id="mc_embed_signup">
-          <form action="https://gmail.us19.list-manage.com/subscribe/post?u=b98f98981dc46f2bf330170ee&amp;id=928f2ce7ef&amp;f_id=00cfc2e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-              <div id="mc_embed_signup_scroll">
-                  <div class="indicates-required">
-                    <span class="asterisk">*</span> indicates required
-                  </div>
-                  <div class="mc-field-group">
-                    <label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label>
-                    <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value="">
-                  </div>
-                  <div class="mc-field-group">
-                    <label for="mce-FNAME">First Name </label>
-                    <input type="text" name="FNAME" class="text" id="mce-FNAME" value="">
-                  </div>
-                  <div class="mc-field-group">
-                    <label for="mce-LNAME">Last Name </label>
-                    <input type="text" name="LNAME" class="text" id="mce-LNAME" value="">
-                  </div>
-                  <div class="mc-address-group">
-                    <div class="mc-field-group">
-                      <label for="mce-ADDRESS-addr1">Address </label>
-                      <input type="text" maxlength="70" name="ADDRESS[addr1]" id="mce-ADDRESS-addr1" class="" value="">
-                    </div>
-                    <div class="mc-field-group">
-                      <label for="mce-ADDRESS-addr2">Address Line 2</label>
-                      <input type="text" maxlength="70" name="ADDRESS[addr2]" id="mce-ADDRESS-addr2" value="">
-                    </div>
-                    <div class="mc-address-fields-group">
-                      <div class="mc-field-group fg-city">
-                        <label for="mce-ADDRESS-city">City</label>
-                        <input type="text" maxlength="40" name="ADDRESS[city]" id="mce-ADDRESS-city" class="" value="">
-                      </div>
-                      <div class="mc-field-group fg-state">
-                        <label for="mce-ADDRESS-state">State/Province/Region</label>
-                        <input type="text" maxlength="20" name="ADDRESS[state]" id="mce-ADDRESS-state" class="" value="">
-                      </div>
-                      <div class="mc-field-group fg-postal">
-                        <label for="mce-ADDRESS-zip">Postal / Zip Code</label>
-                        <input type="text" maxlength="10" name="ADDRESS[zip]" id="mce-ADDRESS-zip" class="" value="">
-                      </div>
-                    </div>
-                    <div class="mc-field-group">
-                      <label for="mce-ADDRESS-country">Country</label>
-                      <select name="ADDRESS[country]" id="mce-ADDRESS-country" class="">
-                        <option value="Canada">Canada</option>
-                        <option value="USA" selected="">USA</option>
-                      </select>
-                    </div>
-                  </div>
-              <div id="mce-responses" class="clear foot">
-                  <div class="response" id="mce-error-response" style="display: none;"></div>
-                  <div class="response" id="mce-success-response" style="display: none;"></div>
-              </div>
-          <div style="position: absolute; left: -5000px;" aria-hidden="true">
-              /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
-              <input type="text" name="b_b98f98981dc46f2bf330170ee_928f2ce7ef" tabindex="-1" value="">
-          </div>
-              <div class="optionalParent">
-                  <div class="clear foot">
-                      <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button btn-primary btn-lg" value="Signup">
-                  </div>
-              </div>
-          </div>
-      </form>
       </div>
-      <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script></div>
-      
+    </div>
+  </div>
+  <section>
+    <div class="container mx-auto p-4">
+      <div class="flex flex-col md:flex-row gap-8">
+        <div class="md:w-1/2">
+          <p class="mt-6 mb-6 text-xl">I LIKE TO EXCHANGE PRINTS. THANKS FOR COMING ALONG FOR THE RIDE. WHAT ARE WE DOING HERE? HOPEFULLY THIS MAKES THE EXCHANGE EASIER AND NO ONE SENDS OUT PRINTS AND DOESN'T GET PRINTS BACK. WE ARE GOING TO A DIFFERENT EXCHANGE TYPE WHERE YOU ALL SEND PRINTS TO ME. I SHUFFLE THEM UP AND SEND YOU BACK SOME COOL PRINTS. AS I'M MAKING THIS SITE I HAVE NO IDEA WHAT I'M DOING. LET'S SEE IF I CAN PUT SOME GUIDE LINES FOR THE EXCHANGE BELOW IN A LIST……</p>
         </div>
-      </section>
+        <div class="md:w-1/2">
+          <img src="/images/exchange-wall.jpg" alt="Exchange artwork" class="w-full h-96 object-cover rounded-lg">
+        </div>
+      </div>
+    </div>
+  </section>
+    <section id="newsletter" class="bg-base-200 p-6 rounded-lg">
+        <div class="flex flex-col md:flex-row gap-8">
+            <div class="md:w-1/2">
+                <h2>
+                  WORDS HERE. THE GUIDE LINES ARE BELOW HERE
+                </h2>
+                  <ul class="list-disc list-inside">
+                    <li class="p-4 pb-2">EXCHANGE IN THE MEMBRANE IS AGAIN A MUSIC THEMED EXCHANGE. CHOOSE YOUR OWN ADVENTURE AS TO WHAT THAT MEANS TO YOU</li>
+                    <li class="p-4 pb-2" >SEND ME 20 PRINTS. AS LONG AS THIS DOESN’T GET TOO BIG EVERY0NE WILL GET BACK 20 PRINTS. 19 RANDOM AND ONE FROM ME. IF IT GETS TOO BIG I RESERVE THE RIGHT TO CHANGE THIS. SORRY NOT SORRY.</li>
+                    <li class="p-4 pb-2">PLEASE NO PAPER BIGGER THAN 8X10. I WANT TO DO AN EXHIBIT NIGHT AGAIN AND IT WAS A BITCH TO FIND THE WEIRD SIZED FRAMES.</li>
+                    <li class="p-4 pb-2 uppercase">ONLY TRADITIONAL PRINT TYPES WILL BE ACCEPTED. EXAMPLES OF ACCEPTABLE TYPES ARE SCREEN PRINTING, ENGRAVING, LITHOGRAPHY, INTAGLIO PRINTING, LETTERPRESS, BLOCK PRINTING, AND ROLLER PRINTING. PRINTS MUST BE NUMBERED 1-20. YOU CAN MAKE THE EDITION BIGGER BUT MUST SEND THE NUMBERS 1-20.</li>
+                    <li class="p-4 pb-2">WRITE YOUR INSTAGRAM ON THE BACK OF ALL YOUR PRINTS. HELPS PEOPLE. I’M PEOPLE,</li>
+                    <li class="p-4 pb-2">I THINK THE BEST PART OF GETTING AND GIVING PRINTS IS SHARING WHAT YOU HAVE RECEIVED AND SEEING PEOPLE SHARE YOUR PRINTS. DO THIS, OR DON’T. I ENCOURAGE YOU TO SHARE THOU.</li>
+                    <li class="p-4 pb-2">I KNOW SOME PEOPLE ARE SAINTS AND LOVE TO SEND EXTRA PRINTS AND STICKERS. THIS IS ALLOWED. DO IT. “THOSE WHO SEND EXTRA PRINTS WILL BE THE FIRST TO PULL PERFECT PRINTS” I THINK THAT’S IN THE BEATITUDES. </li>  
+                    <li class="p-4 pb-2">I’M ASKING EVERYONE TO MAIL BY SEPTEMBER 1, 2025. I WONT START SHORTING THE PRINTS TILL THE END OF THE MONTH TO GIVE PEOPLE OUTSIDE THE US TIME FOR THEIR PRINTS TO ARRIVE.</li>
+                    <li class="p-4 pb-2">IF YOU HAVEN’T REACH OUT TO ME ABOUT THE 10 BUCK FEE FOR HELPING WITH THE SHIPPING. NOT REQUIRED BUT APPRECIATED.</li>
+                    <li class="p-4 pb-2">SHARE YOUR PROCESS. TAG ME @ADMtheLINOCUTTER AND OR USE THE HASHTAG #EXCHANGEINTHEMEMBRANE</li>
+                  </ul>
+                  
+            </div>
+            
+            <div class="md:w-1/2">
+                <!-- Mailchimp Embed Form -->
+                <div class="flex justify-center">
+                    <!-- This is a placeholder. Replace with your actual Mailchimp embed code. -->
+                    <div id="mc_embed_shell">
+                        <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
+                        <style type="text/css">
+                            #mc_embed_signup{ false;clear:left; font:14px Helvetica,Arial,sans-serif; max-width: 600px;}
+                            /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                        </style>
+                        <div id="mc_embed_signup">
+                            <form action="https://gmail.us19.list-manage.com/subscribe/post?u=b98f98981dc46f2bf330170ee&amp;id=928f2ce7ef&amp;f_id=00cfc2e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                                <div id="mc_embed_signup_scroll">
+                                    <div class="indicates-required">
+                                        <span class="asterisk">*</span> indicates required
+                                    </div>
+                                    <div class="mc-field-group">
+                                        <label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label>
+                                        <input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="" value="">
+                                    </div>
+                                    <div class="mc-field-group">
+                                        <label for="mce-FNAME">First Name </label>
+                                        <input type="text" name="FNAME" class="text" id="mce-FNAME" value="">
+                                    </div>
+                                    <div class="mc-field-group">
+                                        <label for="mce-LNAME">Last Name </label>
+                                        <input type="text" name="LNAME" class="text" id="mce-LNAME" value="">
+                                    </div>
+                                    <div class="mc-address-group">
+                                        <div class="mc-field-group">
+                                            <label for="mce-ADDRESS-addr1">Address </label>
+                                            <input type="text" maxlength="70" name="ADDRESS[addr1]" id="mce-ADDRESS-addr1" class="" value="">
+                                        </div>
+                                        <div class="mc-field-group">
+                                            <label for="mce-ADDRESS-addr2">Address Line 2</label>
+                                            <input type="text" maxlength="70" name="ADDRESS[addr2]" id="mce-ADDRESS-addr2" value="">
+                                        </div>
+                                        <div class="mc-address-fields-group">
+                                            <div class="mc-field-group fg-city">
+                                                <label for="mce-ADDRESS-city">City</label>
+                                                <input type="text" maxlength="40" name="ADDRESS[city]" id="mce-ADDRESS-city" class="" value="">
+                                            </div>
+                                            <div class="mc-field-group fg-state">
+                                                <label for="mce-ADDRESS-state">State/Province/Region</label>
+                                                <input type="text" maxlength="20" name="ADDRESS[state]" id="mce-ADDRESS-state" class="" value="">
+                                            </div>
+                                            <div class="mc-field-group fg-postal">
+                                                <label for="mce-ADDRESS-zip">Postal / Zip Code</label>
+                                                <input type="text" maxlength="10" name="ADDRESS[zip]" id="mce-ADDRESS-zip" class="" value="">
+                                            </div>
+                                        </div>
+                                        <div class="mc-field-group">
+                                            <label for="mce-ADDRESS-country">Country</label>
+                                            <select name="ADDRESS[country]" id="mce-ADDRESS-country" class="">
+                                                <option value="Canada">Canada</option>
+                                                <option value="USA" selected="">USA</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div id="mce-responses" class="clear foot">
+                                        <div class="response" id="mce-error-response" style="display: none;"></div>
+                                        <div class="response" id="mce-success-response" style="display: none;"></div>
+                                    </div>
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                        /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+                                        <input type="text" name="b_b98f98981dc46f2bf330170ee_928f2ce7ef" tabindex="-1" value="">
+                                    </div>
+                                    <div class="optionalParent">
+                                        <div class="clear foot">
+                                            <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button btn-primary btn-lg" value="Signup">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script><script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   
     <!-- Gallery Section -->
     <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10 mt-10">
